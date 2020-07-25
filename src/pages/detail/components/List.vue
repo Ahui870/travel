1a04af73677bba989248c6159af7e0ead7d09da4
Ahui <2889201356@qq.com>
2020-07-25 16:58:39 +0800

    travel finished

---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="list-item" 
+         v-for='(item, index) of list' 
+         :key="index">
+      <div class="list-item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .list-item-title{
+    width: 100%;
+    height: .8rem;
+    line-height: .8rem;
+    font-size: .32rem;
+    text-indent: .6rem;
+  }
+
+  .list-item-title .item-title-icon{
+    display: inline-block;
+    position: absolute;
+    width: .36rem;
+    height: .36rem;
+    top: .26rem;
+    left: .2rem;
+    background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
+    margin-right: .1rem;
+    background-size: .4rem 3rem;
+  }
+
+  .item-children{
+    padding: 0 .3rem;
+  }
+
+  .item-children .list-item-title{
+    font-size: .24rem;
+  }
+</style>
